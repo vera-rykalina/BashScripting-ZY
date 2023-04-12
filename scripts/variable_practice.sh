2184f1027f0e6de4fafa26423b8c062ef0aa9e42
vera-rykalina <verarykalina@gmail.com>
2023-04-12 21:14:46 +0200

    Add variable_practice.sh

---
+++ scripts/variable_practice.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+employee="Sarah"
+echo "Dear ${employee}"
+
+echo "The board and I would like to thank you for the exellent work that you have been doing over the last few months"
+echo "Keep up the good work, ${employee}"
+
+echo "Best wishes,"
+echo "Karen, CEO"
